ebabccf8d6cd6be2747e2066e6be95c2583f1faa
FICN <11276782+FICN@noreply.gitee.com>
2022-07-04 15:47:35 +0800

    按钮可点击的界面

--- src/clientDemo.java
+++ src/clientDemo.java
@@ -1,27 +1,61 @@
-import java.io.IOException;
-import java.io.OutputStream;
 import java.net.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.io.*;
 import java.util.Scanner;
-public class clientDemo{
+
+import javax.swing.*;
+import javax.swing.border.Border;
+
+public class clientDemo extends JFrame implements ActionListener{
+    JButton send_botton;
+	JTextArea out = new JTextArea(1, 30);   //send box
+	JTextArea in = new JTextArea(15, 30);   //receive box
+	JPanel pan = new JPanel();
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        System.out.println("hi");
+    }
+
+    public clientDemo(){
+        super("ClientServer");    //JFrame(title)
+		Border border = BorderFactory.createLineBorder(Color.orange, 1);
+	    in.setBorder(border);
+	    out.setBorder(border);
+		send_botton = new JButton("Send");
+		send_botton.addActionListener(this);     //onclick
+        
+		pan.setLayout(new FlowLayout());
+		pan.add(in);
+	    pan.add(out);
+		pan.add(send_botton);
+
+		add(pan);
+		setSize(350,370);
+		setVisible(true);
+    }
+
     public static void main(String[] args) {
-        try{
-            Scanner sc = new Scanner(System.in);
+        clientDemo a = new clientDemo();
+        // try{
+        //     Scanner sc = new Scanner(System.in);
 
-            Socket a=new Socket("127.0.0.1",6666);
-            OutputStream put = a.getOutputStream();
+        //     Socket a=new Socket("127.0.0.1",6666);
+        //     OutputStream put = a.getOutputStream();
 
-            String input = sc.nextLine();
-            while(!input.equals("disconnect")){
-                put.write((input+"\n").getBytes());
-                put.flush();
-                input=sc.nextLine();
-            }
-            put.write(input.getBytes());
-            put.flush();
-            a.close();
-            sc.close();
-        }catch(IOException e){
-            System.out.println("Error"+e);
-        }
+        //     String input = sc.nextLine();
+        //     while(!input.equals("disconnect")){
+        //         put.write((input+"\n").getBytes());
+        //         put.flush();
+        //         input=sc.nextLine();
+        //     }
+        //     put.write(input.getBytes());
+        //     put.flush();
+        //     a.close();
+        //     sc.close();
+        // }catch(IOException e){
+        //     System.out.println("Error"+e);
+        // }
     }
 }
--- src/serverDemo.java
+++ src/serverDemo.java
@@ -1,32 +1,63 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.*;
 import java.net.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.io.*;
 
-public class serverDemo {
-    public static void main(String[] args) {
-        BufferedReader clientin;
-        try{
-            ServerSocket a = new ServerSocket(6666);
+import javax.swing.*;
+import javax.swing.border.Border;
+public class serverDemo extends JFrame implements ActionListener{
+	JButton send_botton;
+	JTextArea out = new JTextArea(1, 30);   //send box
+	JTextArea in = new JTextArea(15, 30);   //receive box
+	JPanel pan = new JPanel();
 
-            Socket client=a.accept();
-            System.out.println("connection suc!");
-            System.out.println("IP:"+client.getInetAddress());
-            System.out.println("Port:"+client.getPort());
-
-            InputStream in = client.getInputStream();
-            clientin = new BufferedReader(new InputStreamReader(in));
-
-            String things=clientin.readLine();
-            while(!things.equals("disconnect")){
-                System.out.println(things);
-                things=clientin.readLine();
-            }
-            
-            System.out.println("connection dis!");
-            a.close();
-        }catch(IOException e){
-            System.out.println("Error:"+e);
-        }
+    @Override
+    public void actionPerformed(ActionEvent e) {        //when click the button
+        System.out.println("hello");
     }
+
+    public serverDemo(){      //create UI
+		super("ChatServer");    //JFrame(title)
+		Border border = BorderFactory.createLineBorder(Color.blue, 1);
+	    in.setBorder(border);
+	    out.setBorder(border);
+		send_botton = new JButton("Send");
+		send_botton.addActionListener(this);     //onclick
+        
+		pan.setLayout(new FlowLayout());
+		pan.add(in);
+	    pan.add(out);
+		pan.add(send_botton);
+
+		add(pan);
+		setSize(350,370);
+		setVisible(true);
+    }
+    public static void main(String[] args) {
+        serverDemo a=new serverDemo();
+        // BufferedReader clientin;
+        // try{
+        //     ServerSocket a = new ServerSocket(6666);
+
+        //     Socket client=a.accept();
+        //     System.out.println("connection suc!");
+        //     System.out.println("IP:"+client.getInetAddress());
+        //     System.out.println("Port:"+client.getPort());
+
+        //     InputStream in = client.getInputStream();
+        //     clientin = new BufferedReader(new InputStreamReader(in));
+
+        //     String things=clientin.readLine();
+        //     while(!things.equals("disconnect")){
+        //         System.out.println(things);
+        //         things=clientin.readLine();
+        //     }
+            
+        //     System.out.println("connection dis!");
+        //     a.close();
+        // }catch(IOException e){
+        //     System.out.println("Error:"+e);
+        // }
+    }
+
 }
